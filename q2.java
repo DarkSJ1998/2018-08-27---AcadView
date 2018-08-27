320418a57c7732164a1347aff72377ce5c252b0a
DarkSJ1998 <sarthakjn12@gmail.com>
2018-08-27 20:30:17 +0530

    Add q2.java

---
+++ q2.java
@@ -0,0 +1,20 @@
+class Demo
+{
+	Demo()
+	{
+		System.out.println("// Default Constructor called.");
+	}
+	
+	Demo(int a)
+	{
+		this();							// invoking the default constructor.
+		System.out.println("// Parameterized Constructor called.");
+	}
+}
+class q2
+{
+	public static void main(String args[])
+	{
+		Demo obj = new Demo(10);		// invoking the parameterized constructor.
+	}
+}
